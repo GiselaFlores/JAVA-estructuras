464e65446917fcf19ff11c9d2db1ac57811acbfd
GiselaFlores <gi.flores@bue.edu.ar>
2024-06-23 22:41:25 -0300

    arrayList y Scanner

--- src/main/java/ar/com/codoacodo/App.java
+++ src/main/java/ar/com/codoacodo/App.java
@@ -1,9 +1,9 @@
 package ar.com.codoacodo;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
@@ -232,5 +232,103 @@ public class App
         Integer[] numeros = {1, 5, 9};
         
         String [] pLatinos = {"Argentina", "Bolivia", "Colombia", "Ecuador"};
+
+
+        //ArrayList - objeto - sin límite
+
+        //Link a metodos de arrayList: https://www.w3schools.com/java/java_ref_arraylist.asp
+
+        //importación de su clase
+
+        // import java.util.ArrayList; Lo declaramos en la parte superior
+
+        // declaramos el objeto de tipo arrayList
+
+        ArrayList<String> paisesLatinosArray = new ArrayList<String>();
+
+        paisesLatinosArray.add("Argetnina");
+        paisesLatinosArray.add("Bolivia");
+        paisesLatinosArray.add("Chile");
+        paisesLatinosArray.add("Colombia");
+
+        System.out.println(paisesLatinosArray);
+
+        // agregar un elemento al principio
+
+        paisesLatinosArray.add(0, "Perú"); 
+
+        System.out.println(paisesLatinosArray);
+
+        //acceder a un aposición particular
+
+        paisesLatinosArray.get(3);
+
+        if (paisesLatinosArray.get(3) == "Chile"){
+            System.out.println("El pais es: Chile" );
+        }
+
+        //modificar un elemento
+
+        paisesLatinosArray.set(0, "México");
+
+        System.out.println(paisesLatinosArray);
+
+
+        //borrar un elemento
+
+        paisesLatinosArray.remove(0);
+
+        System.out.println(paisesLatinosArray);
+
+        //borrar todo el ArrayList
+
+        //paisesLatinosArray.clear();
+
+
+        //tamaño del arrayList
+
+        System.out.println(paisesLatinosArray.size());
+
+        //Recorrido del arrayList
+
+        for (int i = 0; i < paisesLatinosArray.size(); i++) {
+            System.out.println(paisesLatinosArray.get(i));
+        }
+
+        for (String i : paisesLatinosArray) {
+            System.out.println(i);
+        }
+
+        //Métodos array
+
+        //import java.util.Collections; agregar para usar el metodo sort para ordenar.
+
+        Collections.sort(paisesLatinosArray); 
+
+        System.out.println(paisesLatinosArray);
+
+
+        //Ingreso de datos por consola:
+
+        //Scanner
+
+        //import java.util.Scanner; importar arriba para usar
+
+        Scanner nombrePersona = new Scanner(System.in); //declaro el objeto tipo scanner
+        System.out.println("Ingrese su nombre:");
+
+        String usuario = nombrePersona.nextLine();
+        System.out.println("Bienvenido: " + usuario);
+
+        //otros ingresos:
+
+        /* 
+         nextLine(); texto
+         nextint(); entero
+         nextDouble(); decimal
+
+         Necesitan crear un objeto de tipo Scanner y tener importada la clase.
+         */
+
     }
 }
